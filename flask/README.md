bb9e8e6abe50bd3b6c3496db828839e1cbd2bdbc
vologue <raghav.sp98@gmail.com>
2019-02-06 21:53:19 +0530

    Successful Transfer of data between Flask and vue servers and added new Readme for flask

---
+++ flask/README.md
@@ -0,0 +1,21 @@
+# Flask-Backend
+
+## Setup Virtual Environment
+```
+virtualenv venv
+```
+```
+source venv/bin/activate
+```
+
+###Install flask and 
+```
+pip install flask
+```
+```
+pip install flask-cors
+```
+### Run Server
+```
+python app.py
+```
--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask,request
+from flask_cors import CORS
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+@app.route('/api/authenticate', methods=['POST'])
+def auth():
+   data = request.get_json()
+   user=data['email']
+   pas=data['password']
+   #Authenticate data from the database and return result
+   return "Succes"
 
-@app.route('/')
-def basic():
-   return 'Basic'
-
+@app.route('/api/user/create', methods=['POST'])
+def adduser():
+   data = request.get_json()
+   name, email, password, dob, address, prob=data['name'], data['email'], data['password'], data['DOB'], data['address'], data['prob'] 
+   #Add Data to the database and return status
+   return "Succes"
+   
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
